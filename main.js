60d4fe28471abc65541dde418e4c09f066126c6d
bilelmerhben <53703326+bilelmerhben@users.noreply.github.com>
2019-10-12 17:26:21 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+const pets = [
+    { name: "Max", type: "dog", bornOn: 2018 },
+    { name: "Angel", type: "cat", bornOn: 2015 },
+    { name: "Jasper", type: "dog", bornOn: 2016 }
+  ];
+  
+  //arow
+  const getAge = (pet) => new Date().getFullYear() - pet.bornOn;
+     
+ let petsWithAge = [];
+  pets.forEach((pet)=>{pet.age = getAge(pet), petsWithAge.push(pet)})
+  console.log(petsWithAge);
+  
+
+ const dogs = pets.filter((pet)=>{return pet.type === "dog" })
+  console.log(dogs);
+  
+  const petfinded = pets.find((pet)=>{return pet.name === "Jasper"})
+  console.log("Jasper is " + petfinded.age + " years old");
